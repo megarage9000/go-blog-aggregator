7c2955be347b070902c19aad523505220f2280b7
John <gtom_1984@hotmail.ca>
2025-04-10 14:24:44 -0700

    halfway with following

---
+++ sql/queries/feed_follows.sql
@@ -0,0 +1,19 @@
+-- name: CreateFeedFollow :many
+WITH inserted_feed_follow AS (
+    INSERT INTO feed_follows(id, created_at, updated_at, user_id, feed_id) VALUES (
+        $1,
+        $2,
+        $3, 
+        $4,
+        $5
+    )
+    RETURNING *
+) SELECT 
+    inserted_feed_follow.*,
+    users.name as user_name,
+    feed.name as feed_name
+FROM inserted_feed_follow
+INNER JOIN users
+ON users.id == inserted_feed_follow.user_id
+INNER JOIN feed
+ON feed.id == inserted_feed_follow.feed_id;
---
+++ sql/schema/003_feed_follows.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+
+CREATE TABLE feed_follows (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    user_id UUID UNIQUE NOT NULL,
+    feed_id UUID UNIQUE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (feed_id) REFERENCES feed(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+
+DROP TABLE feed_follows;
+
+
