5c228a1bba4705b62b21b5fe467ef0c6fbfe36b5
megarage9000 <gtom_1984@hotmail.ca>
2025-04-11 13:10:03 -0700

    Alter the user id and feed id constraint

--- sql/schema/003_feed_follows.sql
+++ sql/schema/003_feed_follows.sql
@@ -4,10 +4,11 @@ CREATE TABLE feed_follows (
     id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    user_id UUID UNIQUE NOT NULL,
-    feed_id UUID UNIQUE NOT NULL,
+    user_id UUID NOT NULL,
+    feed_id UUID NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (feed_id) REFERENCES feed(id) ON DELETE CASCADE
+    FOREIGN KEY (feed_id) REFERENCES feed(id) ON DELETE CASCADE,
+    UNIQUE(user_id, feed_id)
 );
 
 -- +goose Down
