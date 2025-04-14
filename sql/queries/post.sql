efb0ba7bfede926568b703c3c961a16e1bbaeaa9
John <gtom_1984@hotmail.ca>
2025-04-14 14:32:30 -0700

    Start working on posts

---
+++ sql/queries/post.sql
@@ -0,0 +1,25 @@
+
+-- name: CreatePost :one
+INSERT INTO post (id, created_at, updated_at, title, url, description, published_at, feed_id)
+VALUES(
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6,
+    $7,
+    $8
+)
+
+RETURNING *;
+
+-- name: GetPostsForUser :many
+WITH user_feed_follows AS (
+    SELECT feed_follows.feed_id
+    FROM feed_follows
+    WHERE user_id = $1
+)
+SELECT post.* FROM post
+INNER JOIN user_feed_follows
+ON user_feed_follows.feed_id = post.feed_id;
---
+++ sql/schema/005_posts.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+
+CREATE TABLE post (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    title TEXT NOT NULL,
+    url TEXT UNIQUE NOT NULL,
+    description TEXT,
+    published_at TIMESTAMP,
+    feed_id UUID NOT NULL,
+    FOREIGN KEY (feed_id) REFERENCES feed(id) ON DELETE CASCADE
+);
